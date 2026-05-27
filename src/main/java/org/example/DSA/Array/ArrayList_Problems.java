82c6780fe755a6d1b49bc3d13261c54db82ea56f
shivam7892 <shivam.cse7892@gmail.com>
2026-05-27 14:09:20 +0530

    Array problems with Time complexity and Theory

---
+++ src/main/java/org/example/DSA/Array/ArrayList_Problems.java
@@ -0,0 +1,4 @@
+package org.example.DSA.Array;
+
+public class ArrayList_Problems {
+}
---
+++ src/main/java/org/example/DSA/Array/Array_Problems.java
@@ -0,0 +1,27 @@
+package org.example.DSA.Array;
+
+import java.util.Arrays;
+
+public class Array_Problems {
+
+
+    public static void main(String[] args) {
+
+
+
+        int[] rollNo;  // declaration of array, rollNo is defined in the stack.
+        rollNo = new int[5]; //initialisation: here object is being created in the memory (heap)
+        System.out.println(rollNo[0]); // for the type integer array have byDefault value 0;
+        // and for type string we get byDefault null value.
+
+
+        // new is used to create an object so it will create the object of type array...size 5 in the heap memory.
+        // in c++ array elements are in continuous blocks of memory, but in java array objects are in heap and heap
+        // objects are not continuous it depends on the JVM.
+
+
+
+        // Array of Objects
+
+    }
+}
